d98dee290a03dd8e0e333b5b4dff698147c98d9a
mchudasama <mchudasama@logilite.com>
2020-07-13 18:45:11 +0530

    design changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,15 @@
 class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("-------------------------------");
-	System.out.println("-----------Start---------------");
+	System.out.println("****************************");
 	System.out.println("/-----------Start---------------/");
 	System.out.println("Hello, World!");
 	System.out.println("Hello, User");
+	System.out.println("---------personal details ---------");
 	System.out.println("AGE");
 	System.out.println("Address");
+	System.out.println("****************************");
 	System.out.println("/------------END------------/");
-	System.out.println("------------END------------");
 	System.out.println("-------------------------------");
     }
 }
