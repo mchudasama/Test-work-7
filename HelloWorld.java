35e6815f6efe366d56beab000ddc428497a8eaf4
mchudasama <mchudasama@logilite.com>
2020-07-16 11:53:17 +0530

    hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,42 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-	System.out.println("-------------------------------");
-	System.out.println("****************************");
-	System.out.println("---------personal details ---------");
-	System.out.println("****************************");
-	System.out.println("-------------------------------");
-	System.out.println("-------------------------------");
-	System.out.println("****************************");
-	System.out.println("-------------------------------");
-	System.out.println("****************************");
-	System.out.println("/-----------Start---------------/");
-	System.out.println("Hello, World!");
 	System.out.println("Hello, User");
-	System.out.println("-------------------------------");
-	System.out.println("****************************");
-	System.out.println("---------personal details ---------");
-	System.out.println("****************************");
-	System.out.println("-------------------------------");
-	System.out.println("AGE");
-	System.out.println("Address");
-	System.out.println("Contact - No");
-	System.out.println("Country");
-	System.out.println("pincode");
-	System.out.println("Address2");
-	System.out.println("Address3");
-	System.out.println("****************************");
-	System.out.println("/------------END------------/");
-	System.out.println("-------------------------------");
-	System.out.println("****************************");
-	System.out.println("-------------------------------");
-	System.out.println("AGE");
-	System.out.println("Address");
-	System.out.println("Contact - No");
-	System.out.println("Country");
-	System.out.println("-------------------------------");
-	System.out.println("****************************");
-	System.out.println("-------------------------------");
-	System.out.println("****************************");
-	System.out.println("/-----------Start---------------/");
     }
 }
